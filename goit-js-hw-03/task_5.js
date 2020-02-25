5426543882768ced93aec8069a87ba454e65710d
TrishynaAnna <trishka1998anutka@gmail.com>
2020-02-25 22:36:31 +0200

    homework

---
+++ goit-js-hw-03/task_5.js
@@ -0,0 +1,37 @@
+const products = [{
+    name: "Радар",
+    price: 1300,
+    quantity: 4
+},
+    {
+        name: "Сканер",
+        price: 2700,
+        quantity: 3
+    },
+    {
+        name: "Дроид",
+        price: 400,
+        quantity: 7
+    },
+    {
+        name: "Захват",
+        price: 1200,
+        quantity: 2
+    }
+];
+const getAllPropValues = function (arr, prop) {
+    const values = [];
+
+    for (const objects of arr) {
+        if (prop in objects) {
+            values.push(objects[prop]);
+        }
+    }
+    return values;
+};
+
+console.log(getAllPropValues(products, "name"));
+
+console.log(getAllPropValues(products, "quantity"));
+
+console.log(getAllPropValues(products, "category"));
