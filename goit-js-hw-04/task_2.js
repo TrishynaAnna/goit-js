23ef88ca6a17730b17a9e025972a41cadaffe7d7
TrishynaAnna <trishka1998anutka@gmail.com>
2020-02-26 14:48:40 +0200

    edit

--- goit-js-hw-04/task_2.js
+++ goit-js-hw-04/task_2.js
@@ -17,13 +17,13 @@ const invokeInventoryAction = function(itemName, action, object) {
     action.call(object, itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add, inventory);
+invokeInventoryAction("Medkit", inventory.add.bind(inventory) );
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove, inventory);
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
