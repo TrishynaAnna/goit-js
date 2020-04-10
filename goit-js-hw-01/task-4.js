bd29e00e6951023516b75d0ee503f022f4868fe6
TrishynaAnna <trishka1998anutka@gmail.com>
2020-04-10 18:39:12 +0300

    edit

--- goit-js-hw-01/task-4.js
+++ goit-js-hw-01/task-4.js
@@ -1,26 +1,20 @@
 'use strict'
 //Task_4
-const credits=23580;
-const pricePerDroid=3000;
-let totalPrice;
-const countDroid = prompt('Какое количество дроидов вы бы хотели приобрести??');
-if (countDroid === null) {
+const credits = 23580;
+const pricePerDroid = 3000;
+
+const numberOfDroids = prompt('Введите количество дроидов!');
+const totalPrice = pricePerDroid * numberOfDroids;
+const remainder = credits - totalPrice;
+
+if (numberOfDroids === null) {
     console.log('Отменено пользователем!');
 } else {
-    Number(countDroid);
-    if (countDroid > 0) {
-        totalPrice = countDroid * pricePerDroid;
+    if (totalPrice > credits) {
+        console.log('Недостаточно средств на счету!');
     } else {
-        console.log('Неверное значение');
-    }
-}
-if (totalPrice === undefined) {
-    console.log('Не коректное значение totalPrice', totalPrice);
-} else if (credits < totalPrice) {
-    console.log('Недостаточно средств на счету!');
-} else {
-    console.log(
-        `Вы купили ${totalPrice} дроидов, на счету осталось ${credits - totalPrice} кредитов.`,
-    );
-}
+        console.log(
+            `Вы купили ${numberOfDroids} дроидов, на счету осталось ${remainder} кредитов.`,
+        );}}
+
 
--- goit-js-hw-01/task-5.js
+++ goit-js-hw-01/task-5.js
@@ -1,31 +1,38 @@
 'use strict'
 //Task_5
 let deliveryCost;
+let country;
+const China = 'Китай';
 const deliveryToCountry = prompt("Введите страну, куда нужно доставить товар");
-const country = deliveryToCountry.toLowerCase();
-switch (country) {
-    case "Китай":
-        deliveryCost = 100;
-        alert(`Доставка в ${country} будет стоить ${deliveryCost} кредитов`);
-        break;
-    case "Чили":
-        deliveryCost = 250;
-        alert(`Доставка в ${country} будет стоить ${deliveryCost} кредитов`);
-        break;
-    case "Австралия":
-        deliveryCost = 170;
-        alert(`Доставка в ${country} будет стоить ${deliveryCost} кредитов`);
-        break;
-    case "Индия":
-        deliveryCost = 80;
-        alert(`Доставка в ${country} будет стоить ${deliveryCost} кредитов`);
-        break;
-    case "Ямайка":
-        deliveryCost = 120;
-        alert(`Доставка в ${country} будет стоить ${deliveryCost} кредитов`);
-        break;
-    default:
-        alert("В вашей стране доставка не доступна");
+const deliveryCountry = deliveryToCountry.toLowerCase();
+switch (deliveryCountry) {
+case 'китай':
+country = 'Китай';
+deliveryCost = 100;
+break;
+
+case 'Чили':
+country = 'Чили';
+deliveryCost = 250;
+break;
+
+case 'Австралия':
+country = 'Австралию';
+deliveryCost = 170;
+break;
+
+case 'Индия':
+country = 'Индию';
+deliveryCost = 80;
+break;
+
+case 'Ямайка':
+country = 'Ямайку';
+deliveryCost = 120;
+break;
+
+default:
+alert('В вашей стране доставка недоступна!');
 }
 
-
+console.log(`Доставка в ${deliveryCountry} будет стоить ${deliveryCost} кредитов.`);
