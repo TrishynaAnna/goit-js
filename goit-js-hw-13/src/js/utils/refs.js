6d1fc7849975a71068904b8e2197904b8381ff8c
TrishynaAnna <trishka1998anutka@gmail.com>
2020-05-25 17:51:05 +0300

    add

--- goit-js-hw-13/src/js/utils/refs.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-  searchForm: document.querySelector('#search-form'),
-  galleryList: document.querySelector('.gallery-js'),
-  loadMoreBtn: document.querySelector('button[data-action="load more"]'),
-  photoCardImage: document.querySelector('.photo-card'),
-};
-export { refs };
